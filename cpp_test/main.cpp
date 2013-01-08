55b18ce333c073495faec59caab0a5a1e7d309e0
Jose Luis Clares <jlch.social@gmail.com>
2013-01-08 22:53:24 +0100

    Initial Commit

---
+++ cpp_test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpp_test
+//
+//  Created by Jose Luis Clares on 08/01/13.
+//  Copyright (c) 2013 Jose Luis Clares. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
