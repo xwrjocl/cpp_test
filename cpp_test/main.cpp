323fc58e3cbfcebecf51d8f441025c9adf556784
Jose Luis Clares <jlch.social@gmail.com>
2013-01-08 23:04:10 +0100

    more comments

--- cpp_test/main.cpp
+++ cpp_test/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, const char * argv[])
     // insert code here...
     // I'm adding this comment for the github.
     // I'm adding a new comment
+    // more comments
     std::cout << "Hello, World!\n";
     return 0;
 }
